34eb5e6d8f0a629afcf6c6325fc20a1cc972ca24
ibvwvq <ikramova_arina@mail.ru>
2023-05-02 00:56:27 +0300

    Реализация перехода на личный кабинет

--- src/app/components/personal-account/personal-account-routing.module.ts
+++ src/app/components/personal-account/personal-account-routing.module.ts
@@ -1,8 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PersonalAccountComponent } from './personal-account.component';
+import { AuthguardGuard } from 'src/app/authguard.guard';
 
-const routes: Routes = [{ path: '', component: PersonalAccountComponent }];
+const routes: Routes = [{ path: '', component: PersonalAccountComponent ,canActivate: [AuthguardGuard]}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
