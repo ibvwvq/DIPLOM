b1e36e0deaa291c5ca8609228322e904aa856721
ibvwvq <ikramova_arina@mail.ru>
2023-05-04 21:09:27 +0300

    Модуль и компонент страницы создания курса

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,8 @@ const routes: Routes = [
   { path: 'catalog', loadChildren: () => import('./components/catalog/catalog.module').then(m => m.CatalogModule), canActivate: [AuthguardGuard]},
   { path: 'personal-account', loadChildren: () => import('./components/personal-account/personal-account.module').then(m => m.PersonalAccountModule) , canActivate: [AuthguardGuard]},
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'create-promo-course', loadChildren: () => import('./components/create-promo-course/create-promo-course.module').then(m => m.CreatePromoCourseModule) }
+  { path: 'create-promo-course', loadChildren: () => import('./components/create-promo-course/create-promo-course.module').then(m => m.CreatePromoCourseModule) },
+  { path: 'create-course', loadChildren: () => import('./components/create-course/create-course.module').then(m => m.CreateCourseModule) }
 ];
 
 @NgModule({
---
+++ src/app/components/create-course/create-course.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CreateCourseRoutingModule } from './create-course-routing.module';
+import { CreateCourseComponent } from './create-course.component';
+
+
+@NgModule({
+  declarations: [
+    CreateCourseComponent
+  ],
+  imports: [
+    CommonModule,
+    CreateCourseRoutingModule
+  ]
+})
+export class CreateCourseModule { }
