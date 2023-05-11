85b778e498442b9b3065ac7c0ca7e138b248c0c9
ibvwvq <ikramova_arina@mail.ru>
2023-05-11 20:47:28 +0300

    Реализация добавления модуля

--- src/app/components/create-module-course/create-module-course.component.ts
+++ src/app/components/create-module-course/create-module-course.component.ts
@@ -1,16 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { first } from 'rxjs';
+import { ApiService } from 'src/app/services/api/api.service';
+import { InfoCourseService } from 'src/app/services/info-course/info-course.service';
 
 @Component({
   selector: 'app-create-module-course',
   templateUrl: './create-module-course.component.html',
   styleUrls: ['./create-module-course.component.css']
 })
-export class CreateModuleCourseComponent {
+export class CreateModuleCourseComponent implements OnInit {
   formCreateModuleCourse: FormGroup;
-  constructor(private fb:FormBuilder){
+  constructor(private fb: FormBuilder, private dataService: ApiService, private infoCoursesService: InfoCourseService) {
     this.formCreateModuleCourse = this.fb.group({
-      valueNameModule: [null,Validators.required]
+      valueNameModule: [null, Validators.required]
     });
   }
+  idCourse: any;
+  ngOnInit(): void {
+    this.idCourse = this.infoCoursesService.current_course.idCourse;
+  }
+
+  createModule(form: FormGroup) {
+
+    this.dataService.createModule(form.value.valueNameModule, this.idCourse)
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+
+        error => {
+          console.log(error);
+        });
+  }
 }
+
