04955eaa041520407bfe173a2a0eaeb57fe8189f
ibvwvq <ikramova_arina@mail.ru>
2023-05-24 16:00:35 +0300

    ссылка на удаление задачи

--- src/app/components/edit-lesson/edit-lesson.module.ts
+++ src/app/components/edit-lesson/edit-lesson.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { EditLessonRoutingModule } from './edit-lesson-routing.module';
 import { EditLessonComponent } from './edit-lesson.component';
 import {AppbarModule} from "../appbar/appbar.module";
-import {TuiButtonModule, TuiSvgModule} from "@taiga-ui/core";
+import {TuiButtonModule, TuiLinkModule, TuiSvgModule} from "@taiga-ui/core";
 import {TuiForModule} from "@taiga-ui/cdk";
 import {TuiFilterByInputPipeModule, TuiTextAreaModule} from '@taiga-ui/kit';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
@@ -27,7 +27,8 @@ import {TuiComboBoxModule, TuiDataListWrapperModule} from '@taiga-ui/kit';
     TuiDataListWrapperModule,
     TuiComboBoxModule,
     TuiDataListModule,
-    TuiFilterByInputPipeModule
+    TuiFilterByInputPipeModule,
+    TuiLinkModule
   ]
 })
 export class EditLessonModule { }
