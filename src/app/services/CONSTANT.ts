dc6c7f3b0897f4fa6bf1617fc8b8845d74c04419
ibvwvq <ikramova_arina@mail.ru>
2023-06-06 22:28:23 +0300

    создание файла с константами

---
+++ src/app/services/CONSTANT.ts
@@ -0,0 +1,28 @@
+
+
+export const startCodeCsharp:any = 'using System;\n' +
+  'using System.Collections.Generic;\n' +
+  'using System.Linq;\n' +
+  'using System.Text;\n' +
+  'using System.Threading.Tasks;\n' +
+  '\n' +
+  'namespace ConsoleApp\n' +
+  '{\n' +
+  '    internal class Program\n' +
+  '    {\n' +
+  '        static void Main(string[] args)\n' +
+  '        {\n' +
+  '\n' +
+  '        }\n' +
+  '    }\n' +
+  '}\n';
+
+export const startCodeJava:any =
+  'public class Test { \n' +
+  '  \n' +
+  '   public static void main(String[] args) { \n' +
+  '                                           \n' +
+  '  \n' +
+  '      System.out.println("Hello world"); \n' +
+  '   } \n' +
+  '}'
